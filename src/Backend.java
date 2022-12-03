c8d9f91984702708ca4ca89bbaee806589ec14dc
josiah <jvillarante@csus.edu>
2022-12-02 16:56:46 -0800

    Refactored code. Created static methods to connect to database and to display the customer table

--- src/Backend.java
+++ src/Backend.java
@@ -6,10 +6,12 @@ public class Backend {
     public static void main(String[] args) throws SQLException{
         System.out.println("Hello, World!");
 
+        /*
         String url = "jdbc:postgresql://ec2-3-92-98-129.compute-1.amazonaws.com:5432/d2kcc4it2iou00";
         String username = "urcyphloccxygf";
         String password = "16e062b8cc6601f501d1b62aee13c758540331c093d7074f424bc687b6bf5351";
         String query = "select * from store";
+
 
         try {
             Class.forName("org.postgresql.Driver");
@@ -40,6 +42,54 @@ public class Backend {
             PreparedStatement statement = conn.prepareStatement("INSERT INTO merchandise VALUES (?)");
             statement.setString(1, "B");
             statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+         */
+
+        Connection conn = null;
+
+        try {
+            conn = connect();
+            printCustomerTable(conn);
+        } finally {
+            try {
+                if (conn != null) { conn.close(); }
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:postgresql://ec2-3-92-98-129.compute-1.amazonaws.com:5432/d2kcc4it2iou00",
+                    "urcyphloccxygf",
+                    "16e062b8cc6601f501d1b62aee13c758540331c093d7074f424bc687b6bf5351");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void printCustomerTable(Connection conn) {
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery("select * from customer");
+
+            while (result.next()) {
+                String data = "";
+
+                for (int i = 1; i <= 7; i++) {
+                    data += result.getString(i) + ":";
+                }
+                System.out.println(data);
+
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
